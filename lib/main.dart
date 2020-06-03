d1008dd5e62a35ca0dc43db78fa4ffb11280384b
hassanrehman01398 <hassanrehman01398@gmail.com>
2020-06-03 13:40:17 +0500

    Home page added Gamify App

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:gamify/pages/homepage.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gamify',
+      theme: ThemeData(
+        
+      brightness:Brightness.dark,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home:homepage(),
+    );
+  }
+}
---
+++ lib/pages/homepage.dart
@@ -0,0 +1,30 @@
+import "package:flutter/material.dart";
+//stateful widget data can transform its data during developement
+//while stateless widget cannot change data 
+
+class homepage extends StatefulWidget{
+
+@override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+
+}
+//Now what is scaffold?Scaffold is a simple blank page.
+class _MyHomepagestate extends State<homepage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body:Container(
+
+
+      ) ,
+    
+    
+    
+    );
+  }
+}
+
