8f7637e311385ef09ad59969f07bc7b67aee3a9d
hassanrehman01398 <hassanrehman01398@gmail.com>
2020-06-03 14:24:14 +0500

    Home page added Gamify App update 1

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,7 +1,7 @@
 import "package:flutter/material.dart";
 //stateful widget data can transform its data during developement
 //while stateless widget cannot change data 
-
+import "package:gamify/data.dart";
 class homepage extends StatefulWidget{
 
 @override
@@ -13,18 +13,38 @@ class homepage extends StatefulWidget{
 }
 //Now what is scaffold?Scaffold is a simple blank page.
 class _MyHomepagestate extends State<homepage>{
+var device_height;
+var device_width;
+@override
+  void initState() {
+    device_height=MediaQuery.of(context).size.height;
+    device_width=MediaQuery.of(context).size.width;
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body:Container(
-
-
+      //now we will use stack bcz stack will show from top to bottom
+      body:Stack(
+children: [],
       ) ,
     
     
     
     );
+  }
+  Widget _featuegamewidget(){
+
+return SizedBox(height:device_height*0.50,width:device_width,child: Container(
+decoration: BoxDecoration(
+image: DecorationImage(
+  fit: BoxFit.cover,
+  image:NetworkImage(featuredGames[0].coverImage.url) 
+
+)
+),
+));
   }
 }
 
